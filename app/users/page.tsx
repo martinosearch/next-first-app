5dd5d419b37507741394977add06809723accb09
martin <martinosearch@gmail.com>
2024-05-18 22:35:54 +0000

    first commit

---
+++ app/users/page.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+interface User {
+    id: number;
+    name: string;
+    email: string;
+}
+
+const UsersPage = async () => {
+    const resp = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users: User[] = await resp.json();
+
+    return (
+        <>
+            <h1>UsersPage</h1>
+            <table className='table table-bordered'>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map(user => <tr key={user.id}>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td>
+                    </tr>)}
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+export default UsersPage
